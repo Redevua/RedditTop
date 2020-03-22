13811654d59a3c3eeebf91c8136d137f3c2b4174
DanyloBulanov <danylo.bulanov@wefuture.co.uk>
2020-03-22 21:00:57 +0200

    clean

--- Reddit Top/Source/Application/AppServices.swift
+++ Reddit Top/Source/Application/AppServices.swift
@@ -13,27 +13,5 @@ final class AppServices {
     init(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) {
         
     }
-    
-    //       private func setupNetworkListener() {
-    //           ReachabilityService.shared.startListening()
-    //       }
-    //       
-    //       private func setupNotification(application: UIApplication) {
-    //           notificationService = NotificationsService(application: application)
-    //           notificationService.register()
-    //       }
-    //       
-    //       private func setupFacebook(_ application: UIApplication, _ launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
-    //           ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
-    //       }
-    //       
-    //       private func setupShortucts() {
-    //           if ArchiveService.standart.isAuthorized {
-    //               SMShortscutService.shared.register()
-    //           }
-    //       }
-    //       
-    //       private func removedTempFiles() {
-    //           SMFileManager.shared.removeTempFiles()
-    //       }
+
 }
